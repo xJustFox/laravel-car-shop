21be2f0f0740d416563b559e1fab15271e7aaccb
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-02-24 12:11:33 +0100

    changed form and rules in store request, changed form

--- resources/views/admin/cars/create.blade.php
+++ resources/views/admin/cars/create.blade.php
@@ -3,95 +3,131 @@
 @section('content')
 
     <div class="container">
-        <div class="row">
-            <div class="col-12 my-3">
-                <h2 class="text-center">Add a New Car</h2>
+        <div class="card my-5">
+            <div class="card-header">
+                <div class="col-12">
+                    <h2 class="fs-4 my-4 px-3 ">
+                        Add new car
+                    </h2>
+                </div>
             </div>
-            <div class="col-12">
-                <form class="row g-3" action="{{ route('admin.cars.store') }}" method="POST">
-                    @csrf
-
-                    <div class="col-6">
-                        <div class="col-12">
-                            <label for="model" class="form-label">Car Model:</label>
-                            <input type="text" class="form-control" id="model" placeholder="Insert car model...">
-                            @error('model')
-                                <div class="text-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="col-12">
-                            <label for="brand" class="form-label">Car Brand:</label>
-                            <input type="text" class="form-control" id="brand" placeholder="Insert car brand...">
-                            @error('brand')
-                                <div class="text-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="col-12">
-                            <label for="year" class="form-label">Car Year:</label>
-                            <input class="form-control" type="number" min="1900" max="2099" step="1" name="year" id="year" />
-                        </div>
-                        <div class="col-12">
-                            <label for="color" class="form-label">Color:</label>
-                            <input type="text" class="form-control" id="color" placeholder="Insert color...">
-                            @error('color')
-                                <div class="text-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="col-12">
-                            <label for="kilometers" class="form-label">Kilometers</label>
-                            <input type="text" class="form-control" id="color" placeholder="Insert kilometers...">
-                            @error('kilometers')
-                                <div class="text-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="col-12">
-                            <label for="price" class="form-label">Sell Price:</label>
-                            <div class="input-group">
-                                <span class="input-group-text">€</span>
-                                <input type="text" class="form-control" id="price" aria-label="Amount (to the nearest euro)">
-                                <span class="input-group-text">.00</span>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-12">
+                        <form class="row g-3" action="{{ route('admin.cars.store') }}" method="POST">
+                            @csrf
+        
+                            <div class="col-6">
+        
+                                <div class="col-12 py-2">
+                                    <label for="model" class="form-label">Car Model:</label>
+                                    <input name="model" type="text" class="form-control form-control-sm" id="model" placeholder="Insert car model...">
+                                    @error('model')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
+                                </div>
+        
+                                <div class="col-12 py-2">
+                                    <label for="color" class="form-label">Color:</label>
+                                    <input name="color" type="text" class="form-control form-control-sm" id="color" placeholder="Insert color...">
+                                    @error('color')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                                
+                                <div class="col-12 py-2">
+                                    <label for="kilometers" class="form-label">Kilometers</label>
+                                    <input name="kilometers" type="number" class="form-control form-control-sm" id="kilometers" placeholder="Insert kilometers...">
+                                    @error('kilometers')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
+                                </div>
                             </div>
-                            @error('price')
-                                <div class="text-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="col-12">
-                            <label for="transmission" class="form-label">Transmission:</label>
-                            <input type="text" class="form-control" id="transmission" placeholder="Insert car transmission...">
-                            @error('transmission')
-                                <div class="text-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="col-12">
-                            <label for="fuel_type" class="form-label">Fuel Type:</label>
-                            <input type="text" class="form-control"  name="fuel_type" id="fuel_type" placeholder="Insert the type of fuel...">
-                            @error('fuel_type')
-                                <div class="text-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="col-12">
-                            <label for="engine_size" class="form-label">Engine Size:</label>
-                            <input type="text" class="form-control" id="engine_size" placeholder="Insert engine size...">
-                            @error('engine_size')
-                                <div class="text-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="col-12">
-                            <label for="kilometers" class="form-label">Car description:</label>
-                            <div class="form-floating">
-                                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
-                                <label for="floatingTextarea2">Add description</label>
+                            <div class="col-6">
+        
+                                <div class="col-12 py-2">
+                                    <label for="brand" class="form-label">Car Brand:</label>
+                                    <input name="brand" type="text" class="form-control form-control-sm" id="brand" placeholder="Insert car brand...">
+                                    @error('brand')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
+                                </div>
+        
+                                <div class="col-12 py-2">
+                                    <label for="year" class="form-label">Car Year:</label>
+                                    <input class="form-control form-control-sm" type="number" min="1900" max="2099" step="1" name="year" id="year" />
+                                </div>
+        
+                                <div class="col-12 py-2">
+                                    <label for="price" class="form-label">Sell Price:</label>
+                                    <div class="input-group input-group-sm ">
+                                        <span class="input-group-text">€</span>
+                                        <input name="price" type="number" class="form-control form-control-sm" id="price" aria-label="Amount (to the nearest euro)">
+                                        <span class="input-group-text">.00</span>
+                                    </div>
+                                    @error('price')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
+                                </div>
                             </div>
-                        </div>
+                            <div class="col-12 d-flex justify-content-between">
+        
+                                <div class="col-3">
+                                    <label for="transmission" class="form-label">Transmission:</label>
+                                    <div class="input-group mb-3">
+                                        <select class="form-select form-select-sm" name="transmission" id="transmission">
+                                            <option selected>Choose...</option>
+                                            <option value="Manual">Manual</option>
+                                            <option value="Automatic">Automatic</option>
+                                        </select>
+                                    </div>
+                                    @error('transmission')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
+                                </div>
+        
+                                <div class="col-3">
+                                    <label for="fuel_type" class="form-label">Fuel Type:</label>
+                                    <div class="input-group mb-3">
+                                        <select class="form-select form-select-sm" name="fuel_type" id="fuel_type">
+                                            <option selected>Choose...</option>
+                                            <option value="Gasoline">Gasoline</option>
+                                            <option value="Electric">Electric</option>
+                                            <option value="Diesel">Diesel</option>
+                                            <option value="Hybrid">Hybrid</option>
+                                            <option value="Plugin-hybrid">Plugin-hybrid</option>
+                                            <option value="Natural-gas">Natural-gas</option>
+                                        </select>
+                                    </div>
+                                    @error('fuel_type')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                                
+                                <div class="col-3">
+                                    <label for="engine_size" class="form-label ">Engine Size:</label>
+                                    <input name="engine_size" step=".01" type="number" class="form-control form-control-sm" id="engine_size">
+                                    @error('engine_size')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
+                                </div>
+        
+                            </div>
+                            <div class="col-12">
+                                <label for="description" class="form-label">Car description:</label>
+                                <div class="form-floating">
+                                    <textarea name="description" class="form-control form-control-sm" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+                                    <label for="floatingTextarea2">Add description</label>
+                                </div>
+                            </div>
+                            <div class="col-12 text-center mt-4">
+                                <button type="submit" class="btn btn-primary float-end">Add</button>
+                            </div>
+                        </form>
                     </div>
-                    <div class="col-12 text-center mt-4">
-                        <button type="submit" class="btn btn-primary">Aggiungi</button>
-                    </div>
-                </form>
+                </div>
             </div>
-        </div>
+        </div>        
     </div>
 
 @endsection
