de745c6625d7766b2b030f887f3eb05cb3bb65b4
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-02-29 14:17:44 +0100

    added id to cars table

--- app/Http/Controllers/Admin/BrandController.php
+++ app/Http/Controllers/Admin/BrandController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Models\Brand;
 use App\Http\Requests\StoreBrandRequest;
 use App\Http\Requests\UpdateBrandRequest;
