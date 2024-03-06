cf2b2871a5213906c219e8f6d0a1e22baf9cf35b
xJustFox <dodydaversa@gmail.com>
2024-03-06 17:45:16 +0100

    [Fix] Aggiustata indirizzo API Brands

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\CarController;
+use App\Http\Controllers\Api\BrandController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\CarController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +20,7 @@ Route::get('/cars', [CarController::class, 'index']);
 Route::get('/cars/{slug}', [CarController::class, 'show']);
 Route::get('/cars/brand/{slug}', [CarController::class, 'brand_cars']);
 
-Route::get('/brands', [CarController::class, 'index']);
+Route::get('/brands', [BrandController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
